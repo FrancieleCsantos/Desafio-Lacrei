00d0bdd77dd0478bd2ba2b4e886173105cd3e7c7
Fran Santos <104040061+FrancieleCsantos@users.noreply.github.com>
2022-12-14 22:12:50 -0300

    Criado layout pagina Home

--- projeto-lacrei/src/App.js
+++ projeto-lacrei/src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import Footer from './Component/Footer';
 import Header from './Component/Header'
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
       <Footer />
     </div>
   );
--- projeto-lacrei/src/Pages/Home/index.js
+++ projeto-lacrei/src/Pages/Home/index.js
@@ -1,9 +1,25 @@
 import './styles.css'
+import imgHome from '../../assets/img-home.svg'
 
 function Home() {
 
     return (
-        <h1>Home</h1>
+        <div className='general-container'>
+            <div className='left'>
+                <h1>Boas vindas a Lacrei <br />
+                    Saúde</h1>
+
+                <h2>Uma plataforma segura e acolhedora <br />
+                    para comunidade LGBTQIAPN+</h2>
+
+                <div className='general-container-button' >
+                    <button className='btn-green'>Pessoa Usuária</button>
+                    <button className='btn-edge-green'>Profissional</button></div>
+            </div>
+            <div className='right'>
+                <img src={imgHome} alt='img-home' />
+            </div>
+        </div>
     )
 
 }
